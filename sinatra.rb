93a8141c8b8bd03623e5118d225fd736399fd051
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-20 16:04:20 -0600

    add content to erb files

--- sinatra.rb
+++ sinatra.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 require 'pry'
 
+# Create a new basic sinatra project
+# Create the root route GET ‘/’ as your home page for an about me web page
+# Create a GET ‘/contact’ route for a contact me page
+# Create a GET ‘/resume’ route for a resume page
+# Make all views with links to the other routes and style it all pretty like with css.
+
 get '/' do 
 	"About Me"
 end
+
+get '/contact' do 
+	erb :contact
+end
+
+get '/resume' do 
+	erb :resume
+end
+
+
+
+not_found do
+	status 404
+	'not found'
+end
+

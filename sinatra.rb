9961b1e72abf751036222cbe515a0a837cc93b57
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-20 16:41:41 -0600

    added blank index erb

--- sinatra.rb
+++ sinatra.rb
@@ -1,28 +1,26 @@
 require 'sinatra'
 require 'pry'
 
-# Create a new basic sinatra project
-# Create the root route GET ‘/’ as your home page for an about me web page
-# Create a GET ‘/contact’ route for a contact me page
-# Create a GET ‘/resume’ route for a resume page
-# Make all views with links to the other routes and style it all pretty like with css.
 
+# Create the root route GET ‘/’ as your home page for an about me web page
 get '/' do 
-	"About Me"
+	erb :index
 end
 
+
+# Create a GET ‘/contact’ route for a contact me page
 get '/contact' do 
 	erb :contact
 end
 
+
+# Create a GET ‘/resume’ route for a resume page
 get '/resume' do 
 	erb :resume
 end
-
 
 
 not_found do
 	status 404
 	'not found'
 end
-
